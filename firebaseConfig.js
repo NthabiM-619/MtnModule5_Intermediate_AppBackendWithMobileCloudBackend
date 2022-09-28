453808a7c4d489f23b0e6b3cca982e993e013d28
Nthabiseng Mogoane <76831081+NthabiM-619@users.noreply.github.com>
2022-09-29 00:58:51 +0200

    firebaseConfig.js
    React App Firebase Connection

---
+++ firebaseConfig.js
@@ -0,0 +1,13 @@
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD0h0koJAsAZWPnKMXhhG80XGQJdna5Y-4",
+    authDomain: "module5-intermediatereact.firebaseapp.com",
+    projectId: "module5-intermediatereact",
+    storageBucket: "module5-intermediatereact.appspot.com",
+    messagingSenderId: "835706739201",
+    appId: "1:835706739201:web:921a6bc1a28742f64cfe00",
+    measurementId: "G-VL02C9CK72"
+  };
+  
+  export default firebaseConfiguration
