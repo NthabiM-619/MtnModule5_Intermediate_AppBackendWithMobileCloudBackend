49063b4fe64401bddb98388eceb0cbe331cbcef7
Nthabiseng Mogoane <76831081+NthabiM-619@users.noreply.github.com>
2022-09-29 02:26:48 +0200

    App.js
    React main App

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import 'react-native-gesture-handler';
+import Voice from '@react-native-voice/voice';
  
 // Import React and Component
 import React from 'react';
@@ -17,6 +18,12 @@ import RegisterScreen from './Screen/RegisterScreen';
 import DrawerNavigationRoutes from './Screen/DrawerNavigationRoutes';
  
 const Stack = createStackNavigator();
+import * as firebase from 'firebase';
+import firebaseConfig from './firebaseConfig';
+if (!firebase.apps.length) {
+  console.log('Connected with Firebase')
+  firebase.initializeApp(firebaseConfig);
+}
  
 const Auth = () => {
   // Stack Navigator for Login and Sign up Screen
@@ -52,10 +59,12 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="SplashScreen">
+
         {/* SplashScreen which will come once for 5 Seconds */}
         <Stack.Screen
           name="SplashScreen"
           component={SplashScreen}
+
           // Hiding header for Splash Screen
           options={{headerShown: false}}
         />
@@ -69,6 +78,7 @@ const App = () => {
         <Stack.Screen
           name="DrawerNavigationRoutes"
           component={DrawerNavigationRoutes}
+          
           // Hiding header for Navigation Drawer
           options={{headerShown: false}}
         />
